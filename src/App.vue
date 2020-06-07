56aa554d7bd599493d6dcd88971eee08ed21f2fa
Кузьминых Сергей <kuzminyhsa@gmail.com>
2020-06-07 22:14:28 +0800

    дз со слайдером на Vue

--- src/App.vue
+++ src/App.vue
@@ -30,22 +30,31 @@
 
     <div class="container">
       <post v-for="post of posts" :data = "post" v-bind:key="post.id"></post>
+      <slider :photos = "photos"> </slider>
       </div>
   </div>
 </template>
 
 <script>
 import Post from "@/components/Post"
+import Slider from "@/components/Slider"
+
 import fakedata from "./fakedata.json"
 
 export default {
   name: "app",
   components: {
-    Post
+    Post,
+    Slider
   },
   data() {
     return {
-      posts: fakedata.posts
+      posts: fakedata.posts,
+      photos: [
+         "img/posts/js-carousel/00.jpg",
+         "img/posts/js-carousel/01.jpg",
+         "img/posts/js-carousel/02.jpg"
+      ]
     };
   }
 };
---
+++ src/components/Slider.vue
@@ -0,0 +1,38 @@
+<template> 
+    <div class="post__img">
+        <div class="slider owl-theme">
+            <img :src="currentPhoto" alt="Photo">
+        </div>
+        <button @click="prev">prev</button>
+        <button @click="next">next</button> 
+    </div>
+</template> 
+<script>
+    export default {
+     props: ['photos'],
+     data(){
+       return {
+         currentPhoto : '',  
+       i : ''  
+       }
+       
+     },
+     methods: {
+       next() {
+        var j = this.i++
+        console.log('j',j)
+        this.currentPhoto  = this.photos[j] 
+       },
+       prev() {
+        var j = this.i--
+        this.currentPhoto  = this.photos[j] 
+       }    
+     },
+     created() {
+         this.i=0
+         this.currentPhoto  = this.photos[0]
+     }
+     
+    }
+</script>           
+            
